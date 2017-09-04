7a294ab11fa7bde89577148005b9b36100a962f7
Ciubotaru-Bordeianu_Andrei <canapoca@yahoo.com>
2017-09-04 15:37:48 +0200

    _initialCommit

---
+++ Clients Call/ClientsCall/Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour 
+{
+
+    public List<GameObject> Players;
+	// Use this for initialization
+
+    public void CollidedWithPlayer(GameObject who)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
