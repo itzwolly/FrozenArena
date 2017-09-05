20cbc268f7e8c2007859f20fbb2bc1361ca6fbd5
Wolly <wolly.m.b@gmail.com>
2017-09-05 17:44:04 +0200

    Added slowing down tile and breakable wall stuff

---
+++ Clients Call/Assets/Scripts/Level/Tiles/SlowDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : MonoBehaviour {
+    [SerializeField] private float _slowDownMultiplier;
+    [SerializeField] private float _slowedDownSpeed;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.transform.tag == "Player") {
+            SlowDownObject(collision.gameObject, _slowDownMultiplier);
+            SetObjectSpeed(collision.gameObject, _slowedDownSpeed);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if (collision.transform.tag == "Player") {
+            // empty
+            SetObjectSpeed(collision.gameObject, collision.gameObject.GetComponent<PlayerMovement>().Speed);
+        }
+    }
+
+    private void SlowDownObject(GameObject pObject, float pAmount) {
+        pObject.GetComponent<Rigidbody>().velocity *= pAmount;
+    }
+
+    private void SetObjectSpeed(GameObject pObject, float pAmount) {
+        pObject.GetComponent<PlayerMovement>().CurrentSpeed = pAmount;
+    }
+}
