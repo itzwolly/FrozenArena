b7e45f21937fbf7a7f9096c95ef05b6859436bfe
AndreiNew <cancapoca@yahoo.com>
2017-09-14 10:14:50 +0200

    save works

--- Clients Call/Assets/Scripts/Loading/MainScript/LoadSaveLevelScript.cs
+++ Clients Call/Assets/Scripts/Loading/MainScript/LoadSaveLevelScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using DLLLibrary;
 using UnityEngine;
 
 public class LoadSaveLevelScript : MonoBehaviour {
@@ -19,10 +20,10 @@ public class LoadSaveLevelScript : MonoBehaviour {
             File.Create(path);
         }
         File.WriteAllText(path, String.Empty);
-        Utils.WriteToFile(path, "PlayerMass " + _LevelInfo.GetPlayerMass.ToString()+"|");
-        Utils.WriteToFile(path, "BreakableMass " + _LevelInfo.GetBreakableMass.ToString() + "|");
-        Utils.WriteToFile(path, "BouncePower " + _LevelInfo.GetBouncepower.ToString() + "|");
-        Utils.WriteToFile(path, "FrictionValue " + _LevelInfo.GetIcynessValue.ToString() + "|");
+        Utility.WriteToFile(path, "PlayerMass " + _LevelInfo.GetPlayerMass.ToString()+"|");
+        Utility.WriteToFile(path, "BreakableMass " + _LevelInfo.GetBreakableMass.ToString() + "|");
+        Utility.WriteToFile(path, "BouncePower " + _LevelInfo.GetBouncepower.ToString() + "|");
+        Utility.WriteToFile(path, "FrictionValue " + _LevelInfo.GetIcynessValue.ToString() + "|");
         string info;
         bool changed;
         foreach (Transform t in transform)
@@ -36,7 +37,7 @@ public class LoadSaveLevelScript : MonoBehaviour {
             }
             if (t.gameObject.GetComponent<BombTile>() != null)
             {
-                info += "Bomb" + "|"+VectorToString(t.position);
+                info += "Bomb" + "|"+ Utility.VectorToString(t.position);
                 if(changed)
                 {
                     info += "|"+ t.gameObject.GetComponent<BombTile>().PowerMultiplier.ToString();
@@ -44,73 +45,58 @@ public class LoadSaveLevelScript : MonoBehaviour {
                     info += "|"+ t.gameObject.GetComponent<BombTile>().ExplodeTimer.ToString();
                     info += "|"+ t.gameObject.GetComponent<BombTile>().ResetTime.ToString();
                 }
-                Utils.WriteToFile(path, info);
+                Utility.WriteToFile(path, info);
             }
             else if (t.tag == "Player")
             {
-                info += "Player" + "|" + VectorToString(t.position);
-                Utils.WriteToFile(path,info);
+                info += "Player" + "|" + Utility.VectorToString(t.position);
+                Utility.WriteToFile(path,info);
             }
             else if (t.gameObject.GetComponent<Rigidbody>() != null)
             {
-                info += "Breakable" + "|" + VectorToString(t.position);
+                info += "Breakable" + "|" + Utility.VectorToString(t.position);
                 if(changed)
                 {
                     info += "|"+t.gameObject.GetComponent<Rigidbody>().mass.ToString();
                 }
-                Utils.WriteToFile(path, info);
+                Utility.WriteToFile(path, info);
             }
             else if (t.gameObject.GetComponent<MultiDirectionalBoost>() != null)
             {
-                info += "MultiDirectionalBoost" + "|" + VectorToString(t.position);
+                info += "MultiDirectionalBoost" + "|" + Utility.VectorToString(t.position);
                 if(changed)
                 {
                     info += "|"+t.gameObject.GetComponent<MultiDirectionalBoost>().SpeedBoost.ToString();
                 }
-                Utils.WriteToFile(path, info);
+                Utility.WriteToFile(path, info);
             }
             else if (t.gameObject.GetComponent<OneWayBoost>() != null)
             {
-                info += "OneWayBoost" + "|" + VectorToString(t.position);
+                info += "OneWayBoost" + "|" + Utility.VectorToString(t.position);
                 if (changed)
                 {
                     info += "|"+t.gameObject.GetComponent<OneWayBoost>().SpeedBoost.ToString()+"|";
                     info += ((int)(t.gameObject.GetComponent<OneWayBoost>().Direction)).ToString();
                 }
-                Utils.WriteToFile(path, info);
+                Utility.WriteToFile(path, info);
             }
             else if (t.gameObject.GetComponent<SlowDown>() != null)
             {
-                info += "SlowBlock" + "|" + VectorToString(t.position);
+                info += "SlowBlock" + "|" + Utility.VectorToString(t.position);
                 if(changed)
                 {
                     info += "|"+t.gameObject.GetComponent<SlowDown>().SlowDownMultiplier.ToString()+ "|";
                     info += t.gameObject.GetComponent<SlowDown>().SlowedDownSpeed.ToString();
                 }
-                Utils.WriteToFile(path, info);
+                Utility.WriteToFile(path, info);
             }
             else
             {
-                info += "NormalBlock" + "|" + VectorToString(t.position);
-                Utils.WriteToFile(path, info);
+                info += "NormalBlock" + "|" + Utility.VectorToString(t.position);
+                Utility.WriteToFile(path, info);
             }
         }
     }
 
-    public string VectorToString(Vector3 vec)
-    {
-        string words = vec.x + "," + vec.y + "," + vec.z;
-
-        return words;
-    }
-    public Vector3 StringToVector(string str)
-    {
-        Vector3 vec = new Vector3();
-        string[] split = str.Split('(', ',');
-        vec.x = Convert.ToSingle(split[0]);
-        vec.y = Convert.ToSingle(split[1]);
-        vec.z = Convert.ToSingle(split[2]);
-        return vec;
-    }
 
 }
--- DLL/DLLLibrary/DLLLibrary/Utility.cs
+++ DLL/DLLLibrary/DLLLibrary/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,6 +18,149 @@ namespace DLLLibrary
         {
             Scene scene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(scene.name);
+        }
+        public static void WriteToFile(string path, string words)
+        {
+            //Debug.Log("Written to file");
+            StreamWriter write = new StreamWriter(path, true);
+            write.WriteLine(words);
+            write.Close();
+        }
+        public static string VectorToString(Vector3 vec)
+        {
+            string words = vec.x + "," + vec.y + "," + vec.z;
+
+            return words;
+        }
+        public static Vector3 StringToVector(string str)
+        {
+            Vector3 vec = new Vector3();
+            string[] split = str.Split('(', ',');
+            vec.x = Convert.ToSingle(split[0]);
+            vec.y = Convert.ToSingle(split[1]);
+            vec.z = Convert.ToSingle(split[2]);
+            return vec;
+        }
+
+        public static void ReplaceLineFromFile(string path, string words, string stringToReplace)
+        {
+
+            StreamReader reader = new StreamReader(path);
+
+            //int lineNumber=0;
+            string[] file = File.ReadAllText(path).Split('\n', '\r');
+            string[] toWrite = new string[file.Length];
+            int lenght = 0;
+            /**/
+            foreach (string s in file)
+            {
+                if (s != "")
+                {
+                    toWrite[lenght] = s;
+                    lenght++;
+                }
+            }
+            /**/
+            reader.Close();
+
+
+            for (int i = 0; i < lenght; i++)
+            {
+                //Debug.Log(toWrite[i] + "|" + stringToReplace+"|");
+                if (String.Compare(toWrite[i], stringToReplace) == 0)
+                {
+                    //Debug.Log("replacing");
+                    toWrite[i] = words;
+                }
+            }
+            File.WriteAllText(path, String.Empty);
+
+
+            StreamWriter writer = new StreamWriter(path);
+            for (int i = 0; i < lenght; i++)
+            {
+                writer.WriteLine(toWrite[i]);
+            }
+            writer.Close();
+        }
+
+        public static int GetLastNumberFromFile(string path)
+        {
+            string text = ReadFromFile(path);
+
+            StreamReader reader = new StreamReader(path);
+            string[] file = reader.ReadToEnd().Split('\n', '\r', ' ');
+            int latestNumber = 0;
+            int tempNr;
+            for (int i = 0; i < file.Length; i++)
+            {
+                //Debug.Log(file[i]);
+                if (IsNumeric(file[i]))
+                {
+                    latestNumber = Convert.ToInt32(file[i]);
+                }
+            }
+            reader.Close();
+
+            return latestNumber;
+        }
+
+        public static bool IsNumeric(string text)
+        {
+            float value;
+            return float.TryParse(text, out value);
+        }
+
+        public static string ReadFromFile(string path)
+        {
+            StreamReader read = new StreamReader(path);
+            string file = read.ReadToEnd();
+            read.Close();
+            return file;
+        }
+
+
+        public static void SaveStats(string path, string info)
+        {
+            if (ReadFromFile(path).Length > 1)
+            {
+                File.WriteAllText(path, String.Empty);
+            }
+
+            WriteToFile(path, info);
+        }
+
+        public static int GetValueAfterString(string path, string pString)
+        {
+            string text = ReadFromFile(path);
+
+            StreamReader reader = new StreamReader(path);
+            string[] file = reader.ReadToEnd().Split('\n', '\r', ' ');
+            int latestNumber = 0;
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (string.Compare(file[i], pString) == 0)
+                {
+                    if (IsNumeric(file[i + 1]))
+                    {
+                        latestNumber = Convert.ToInt32(file[i + 1]);
+                        i++;
+                    }
+                }
+            }
+            reader.Close();
+
+            return latestNumber;
+        }
+
+        public static void SetValueAfterString(string path, string pString, int nextValue)
+        {
+            int lastValue = GetValueAfterString(path, pString);
+            string before = pString + " " + lastValue;
+            string after = pString + " " + nextValue;
+            //Debug.Log(before + "|" + after);
+            //Debug.Log("|" + before +"|"+ after + "|");
+            ReplaceLineFromFile(path, after, before);
         }
     }
 }
