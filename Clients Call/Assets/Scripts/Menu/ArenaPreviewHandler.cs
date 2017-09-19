b847baaaa2924f278aa1fb0b20d60a80e021674f
WOLLY-PC\Wolly <wolly.b@hotmail.com>
2017-09-20 00:29:27 +0200

    Worked on arena select. Still have to implement the newly created map data into the scene (actual selectable objects, but also other data like name etc.). And actually queueing op maps to be played.

---
+++ Clients Call/Assets/Scripts/Menu/ArenaPreviewHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ArenaPreviewHandler : MonoBehaviour {
+    [SerializeField] Image _previewImage;
+    [SerializeField] Text _mapName;
+    [SerializeField] Text _containing;
+    [SerializeField] Text _difficulty;
+
+    [SerializeField] EventSystem _eventSystem;
+    [SerializeField] KeyCode _interactionKey;
+
+    private GameObject _selected = null;
+
+    private void Start() {
+
+    }
+
+    // Update is called once per frame
+    private void Update () {
+        if (_eventSystem.currentSelectedGameObject != null) {
+            if (_selected != _eventSystem.currentSelectedGameObject) {
+
+                _selected = _eventSystem.currentSelectedGameObject;
+                if (_selected.GetComponent<MapData>() != null) {
+                    MapData data = _selected.GetComponent<MapData>();
+                    _previewImage.sprite = data.Image;
+                    _mapName.text = data.Name;
+                    _containing.text = "Special Tiles: ";
+                    for (int i = 0; i < data.Tiles.Length; i++) {
+                        _containing.text += data.Tiles[i] + ", ";
+                    }
+                    _difficulty.text = "Difficulty: " + data.Difficulty.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ Clients Call/Assets/Scripts/Menu/MapData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour {
+    [SerializeField] private Sprite _image;
+    [SerializeField] private string _name;
+    [SerializeField] private string[] _tiles;
+    [SerializeField] private LevelConfig.LevelDifficulty _difficulty;
+
+    public Sprite Image {
+        get { return _image; }
+    }
+    public string Name {
+        get { return _name; }
+    }
+    public string[] Tiles {
+        get { return _tiles; }
+    }
+    public LevelConfig.LevelDifficulty Difficulty {
+        get { return _difficulty; }
+    }
+}
